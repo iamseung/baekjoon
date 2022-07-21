220a20153e4898173d15ce3da703821c290b0915
이승석 <106569538+iamseung@users.noreply.github.com>
2022-07-21 17:36:19 +0900

    Update A＋B.java

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -4,10 +4,14 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
+// BufferedReader 의 경우 문자열을 받는 대표적인 방법은 readLine()과 read()이다.
+// readLine()은 한 행을 읽어오고, read()는 한 문자만 일겅온다.
+// 특별한 경우가 없는 한 대부분 readLine()을 사용
 public class Main 
 {
 	public static void main(String[] args) throws IOException
     {
+		// System.in 은 입력한 값을 Byte 단위로 읽는 것을 뜻한다.
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String str = br.readLine();
@@ -23,6 +27,9 @@ public class Main
 // 	public static void main(String[] args) 
 //     {
 // 		Scanner sc = new Scanner(System.in);
+//	   // sc.next()     : 문자열 입력시 공백 전까지만 받음
+//	   // sc.nextLine() : 문자열 입력시 Enter 입력 전까지만 받음
+//         // 대부분 문제에서 행 단위로 입력받는 일이 다수이니 문자열 입력 형태로는 sc.nextLine() 을 사용
 //         int A = sc.nextInt();
 //         int B = sc.nextInt();
 //         System.out.println(A+B);
