23824f0e57e8e99b320a98e50c0b7c0045717d4f
이승석 <106569538+iamseung@users.noreply.github.com>
2022-07-22 23:16:15 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 108 ms, Memory: 12872 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+
+		int year = in.nextInt();
+
+		System.out.println(year - 543);
+
+	}
+}
