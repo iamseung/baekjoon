21c174141411b88abb77b180be55b1505a08726b
이승석 <106569538+iamseung@users.noreply.github.com>
2022-07-27 23:42:08 +0900

    [Bronze V] Title: N 찍기, Time: 1436 ms, Memory: 34036 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int a = Integer.parseInt(br.readLine());
+
+		for (int i = 1; i <= a; i++) {
+			System.out.println(i);
+		}
+	}
+}
