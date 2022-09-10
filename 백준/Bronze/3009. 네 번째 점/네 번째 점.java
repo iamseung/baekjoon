c1c0eb1f8008ec5765a1bbf27d3bd265e02f371d
이승석 <106569538+iamseung@users.noreply.github.com>
2022-09-10 21:42:16 +0900

    [Bronze III] Title: 네 번째 점, Time: 76 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        
+		int[] coord_1 = { Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()) };
+		st = new StringTokenizer(br.readLine()," ");
+		int[] coord_2 = { Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()) };
+		st = new StringTokenizer(br.readLine()," ");
+		int[] coord_3 = { Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()) };		
+		
+		int x;
+		int y;
+ 
+		// x 좌표 비교
+		// 1번 x좌표와 2번 x좌표 비교
+		if (coord_1[0] == coord_2[0]) {
+			x = coord_3[0];
+		}
+		// 1번 x좌표와 3번 x좌표 비교
+		else if (coord_1[0] == coord_3[0]) {
+			x = coord_2[0];
+		}
+		// 2번 x좌표와 3번 x좌표 비교
+		else {
+			x = coord_1[0];
+		}
+ 
+		// y 좌표 비교
+		// 1번 x좌표와 2번 x좌표 비교
+		if (coord_1[1] == coord_2[1]) {
+			y = coord_3[1];
+		}
+		// 1번 x좌표와 3번 x좌표 비교
+		else if (coord_1[1] == coord_3[1]) {
+			y = coord_2[1];
+		}
+		// 2번 x좌표와 3번 x좌표 비교
+		else {
+			y = coord_1[1];
+		}
+		
+		System.out.println(x + " " + y);
+		
+	}
+ 
+}
