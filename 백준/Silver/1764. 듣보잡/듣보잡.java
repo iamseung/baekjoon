d4abff0036ab452590fd595ae1dd8cd506150948
이승석 <106569538+iamseung@users.noreply.github.com>
2022-11-10 15:59:24 +0900

    [Silver IV] Title: 듣보잡, Time: 360 ms, Memory: 25488 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main 
+{ 
+	public static void main(String[] args) throws IOException 
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int count = 0;
+		
+		// N 집합
+		HashMap<String, Integer> map = new HashMap<>();
+
+		// 출력할 집합
+		ArrayList<String> list = new ArrayList<>();
+
+		while(N-- >0)
+		{
+			map.put(br.readLine(), 0);
+		}
+		
+		while(M-- >0)
+		{
+			String key = br.readLine();
+
+			// N 과 M 에 모두 있으면 list 에 추가하고 count + 1
+			if(map.containsKey(key))
+			{
+				list.add(key);
+				count++;
+			}
+		}
+
+		// 사전순으로 출력해야 하니 정렬
+		Collections.sort(list);
+
+		// 출력
+		System.out.println(count);
+
+		for (String k : list)
+        {
+            System.out.println(k);
+        }
+		
+	}
+}
