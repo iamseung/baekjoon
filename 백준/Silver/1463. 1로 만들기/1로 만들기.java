0a3f53139a72d597de5815e1f0fbd7ea5d35cc24
이승석 <106569538+iamseung@users.noreply.github.com>
2022-11-28 23:32:37 +0900

    [Silver III] Title: 1로 만들기, Time: 224 ms, Memory: 79424 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class Main 
+{ 
+	static Integer[] dp;
+
+	public static void main(String[] args) throws IOException 
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int N = Integer.parseInt(br.readLine());
+ 
+		dp = new Integer[N + 1];
+		
+		// 0과 1의 경우는 카운트가 0이기 때문에
+		dp[0] = dp[1] = 0;
+ 
+		System.out.print(recur(N));
+	}
+ 
+	// 모든 경우를 dp[N]에 저장해 줌으로써 메모제이션 사용
+	static int recur(int N) 
+	{
+		if (dp[N] == null) 
+		{
+			// 6으로 나눠지는 경우 
+			if (N % 6 == 0) 
+			{
+				dp[N] = Math.min(recur(N - 1), Math.min(recur(N / 3), recur(N / 2))) + 1;
+			}
+			// 3으로만 나눠지는 경우 
+			else if (N % 3 == 0) 
+			{
+				dp[N] = Math.min(recur(N / 3), recur(N - 1)) + 1;
+			}
+			// 2로만 나눠지는 경우 
+			else if (N % 2 == 0) 
+			{
+				dp[N] = Math.min(recur(N / 2), recur(N - 1)) + 1;
+			}
+			// 2와 3으로 나누어지지 않는 경우
+			else 
+			{
+				dp[N] = recur(N - 1) + 1;
+			}
+		}
+		return dp[N];
+	}
+}
