d73c7bc0c1121c09510218d1e2fb35604056b7e0
이승석 <106569538+iamseung@users.noreply.github.com>
2023-02-13 16:32:55 +0900

    [Silver IV] Title: 듣보잡, Time: 324 ms, Memory: 26328 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,10 +1,5 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.StringTokenizer;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.*;
+import java.util.*;
 
 public class Main 
 { 
