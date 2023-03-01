a6555ef2509fcb5f85f1c235725bbad15eca5212
이승석 <106569538+iamseung@users.noreply.github.com>
2023-03-01 23:51:00 +0900

    [Silver III] Title: 수열, Time: 280 ms, Memory: 23896 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.java
@@ -0,0 +1,100 @@
+/*
+ 산성 용액과 알칼리성 용액의 특성값이 주어졌을 때, 이 중 두 개의 서로 다른 용액을 혼합하여 
+ 특성값이 0에 가장 가까운 용액을 만들어내는 두 용액을 찾는 프로그램을 작성하시오.
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class Main 
+{
+    static StringBuilder sb = new StringBuilder();
+    static FastReader scan  = new FastReader();
+
+    static int N,K;
+    static int[] A;
+
+    static void input() 
+    {
+        N = scan.nextInt();
+        K = scan.nextInt();
+        A = new int[N+1];
+
+        for(int i=1; i<=N; i++)
+        {
+            A[i] = scan.nextInt();
+        }
+    }
+
+    static void pro() 
+    {
+        int R=0, ans = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int L=1; L+K-1<=N; L++)
+        {
+            sum -= A[L-1];
+
+            // while(L+R <= K)
+            while(R+1 <= L+K-1)
+            {
+                sum += A[++R];
+            }
+
+            ans = Math.max(sum, ans);
+        }
+
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) 
+    {
+        input();
+        pro();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
