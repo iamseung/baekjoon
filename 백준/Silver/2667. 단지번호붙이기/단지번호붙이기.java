11d10efed39dc1abc077a4fd94f1b90e3764f79a
이승석 <106569538+iamseung@users.noreply.github.com>
2023-03-12 23:28:33 +0900

    [Silver I] Title: 단지번호붙이기, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -0,0 +1,124 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main 
+{
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, group_cnt;
+    static String[] a;
+    static boolean[][] visit;
+    static int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}}; // 상하좌우로 한칸씩 이동한 좌표를 비교하기 위함
+    static ArrayList<Integer> group;
+
+    static void input() 
+    {
+        N = scan.nextInt();
+        a = new String[N];
+        for (int i = 0; i < N; i++)
+            a[i] = scan.nextLine();
+        visit = new boolean[N][N];
+    }
+
+    // x, y 를 갈 수 있다는 걸 알고 방문한 상태
+    static void dfs(int x, int y) 
+    {
+        // 방문했으니 group_cnt를 증가시키고 visit = true
+        group_cnt++;
+        visit[x][y] = true;
+        for (int k = 0; k < 4; k++) 
+        {
+            // x,y 에서 상하좌우로 한칸씩 이동한 좌표를 탐색할 수 있는 지 확인 후, 가능하다면 탐색 진행
+            int nx = x + dir[k][0];
+            int ny = y + dir[k][1];
+            
+            // 지도를 벗어나는 곳으로 가는가?
+            if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;  
+
+            // 갈 수 있는 칸인지 확인해야 한다.
+            if (a[nx].charAt(ny) == '0') continue;  
+
+            // 이미 방문한 적이 있는 곳인가?
+            if (visit[nx][ny]) continue;  
+
+            dfs(nx, ny);
+        }
+    }
+
+    static void pro() {
+        group = new ArrayList<>();
+        for (int i = 0; i < N; i++) 
+        {
+            for (int j = 0; j < N; j++) 
+            {
+                if (!visit[i][j] && a[i].charAt(j) == '1') 
+                {
+                    // 갈 수 있는 칸인데, 이미 방문처리 된, 즉 새롭게 만난 단지인 경우!
+                    group_cnt = 0;
+                    dfs(i, j);
+                    group.add(group_cnt);
+                }
+            }
+        }
+
+        Collections.sort(group); // 오름차순 정렬
+        sb.append(group.size()).append('\n');
+        for (int cnt: group) sb.append(cnt).append('\n');
+        System.out.println(sb.toString());
+    }
+
+    public static void main(String[] args) 
+    {
+        input();
+        pro();
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
