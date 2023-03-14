c87db49fe7c137d035ff287bea56f626054f2ed4
이승석 <106569538+iamseung@users.noreply.github.com>
2023-03-14 22:36:15 +0900

    [Silver I] Title: RGB거리, Time: 136 ms, Memory: 14532 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,63 +1,91 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
+/*
+ * 한자리씩 늘려가며 셀 수 있는 횟수를 저장해가는
+ */
+import java.io.*;
+import java.util.*;
 
-public class Main
+public class Main 
 {
-	final static int Red = 0;
-	final static int Green = 1;
-	final static int Blue = 2;
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
 
-	static int[][] Cost;
-	static int[][] DP;
-	public static void main(String[] args) throws IOException
-	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N;
+    static int[][] Dy;
+    static int[] A;
 
-		int N = Integer.parseInt(br.readLine());
+    static void input() 
+    {
+        N  = scan.nextInt();
+        Dy = new int[N+1][3];
+        A  = new int[3];
+    }
 
-		Cost = new int[N][3];
-		DP   = new int[N][3];
-		
-		StringTokenizer st;
-		for(int i = 0; i < N; i++) 
-		{	
-			st = new StringTokenizer(br.readLine(), " ");
-			
-			Cost[i][Red]   = Integer.parseInt(st.nextToken());
-			Cost[i][Green] = Integer.parseInt(st.nextToken());
-			Cost[i][Blue]  = Integer.parseInt(st.nextToken());
-		}
+    static void pro() 
+    {
+        // Dy[i][j] = 1 ~ i번 집까지 칠했고, i번 집을 j번 색으로 칠하는 경우 중 최소 비용!
+        Dy[0][0] = Dy[0][1] = Dy[0][2] = 0;
 
-		// DP의 첫번째 값(집)은 각 색상비용의 첫번째 값으로 초기화
-		DP[0][Red]   = Cost[0][Red];
-		DP[0][Green] = Cost[0][Green];
-		DP[0][Blue]  = Cost[0][Blue];
+        for(int i = 1; i <= N; i++)
+        {
+            for(int j=0; j<3; j++) A[j] = scan.nextInt();
+            Dy[i][0] = Math.min(Dy[i-1][1], Dy[i-1][2]) + A[0];
+            Dy[i][1] = Math.min(Dy[i-1][0], Dy[i-1][2]) + A[1];
+            Dy[i][2] = Math.min(Dy[i-1][1], Dy[i-1][0]) + A[2];
+        }
 
-		System.out.println(Math.min(Paint_cost(N- 1, Red), Math.min(Paint_cost(N - 1, Green), Paint_cost(N - 1, Blue))));
-	}
+        System.out.println(Math.min(Math.min(Dy[N][0], Dy[N][1]), Dy[N][2]));
+    }
 
-	static int Paint_cost(int N, int color) 
-	{	
-		// 만약 탐색하지 않은 배열이라면
-		if(DP[N][color] == 0) 
-		{	
-			// color의 색에 따라 이전 집의 서로 다른 색을 재귀호출하여 최솟값과 현재 집의 비용을 더해서 DP에 저장한다
-			if(color == Red) 
-			{
-				DP[N][Red] = Math.min(Paint_cost(N - 1, Green), Paint_cost(N - 1, Blue)) + Cost[N][Red];
-			}
-			else if(color == Green) 
-			{
-				DP[N][Green] = Math.min(Paint_cost(N - 1, Red), Paint_cost(N - 1, Blue)) + Cost[N][Green];
-			}
-			else 
-			{
-				DP[N][Blue] = Math.min(Paint_cost(N - 1, Red), Paint_cost(N - 1, Green)) + Cost[N][Blue];
-			}
-			
-		}
-		return DP[N][color];
-	}
+    public static void main(String[] args) 
+    {
+        input();
+        pro();
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
 }
