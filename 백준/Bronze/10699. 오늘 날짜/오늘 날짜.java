176337ec3d800c22930c7931a3028057691fd223
이승석 <106569538+iamseung@users.noreply.github.com>
2023-04-02 23:32:10 +0900

    [Bronze V] Title: 오늘 날짜, Time: 268 ms, Memory: 18792 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.*;
+import java.text.SimpleDateFormat;
+
+public class Main 
+{
+    static FastReader scan = new FastReader();
+
+    public static void main(String[] args) 
+    {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar c1 = Calendar.getInstance();
+	    String strToday = sdf.format(c1.getTime());
+
+        System.out.println(strToday);
+    }
+
+    static class FastReader 
+    {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() 
+        {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException 
+        {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() 
+        {
+            while (st == null || !st.hasMoreElements()) 
+            {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } 
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() 
+        {
+            String str = "";
+            try 
+            {
+                str = br.readLine();
+            } 
+            catch (IOException e) 
+            {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
