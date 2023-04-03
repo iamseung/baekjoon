89ffc6af926fc7b134aae4c91075794a24458112
이승석 <106569538+iamseung@users.noreply.github.com>
2023-04-03 23:43:04 +0900

    [Bronze V] Title: 등록, Time: 116 ms, Memory: 14040 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.util.*;
+import java.text.SimpleDateFormat;
+
+public class Main 
+{
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static void input() 
+    {
+        System.out.println("219");
+        System.out.println("seungseok");
+    }
+
+    static void pro() 
+    {
+        
+    }
+
+    public static void main(String[] args) 
+    {
+        input();
+        pro();
+    }
+
+    static class FastReader 
+    {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() 
+        {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException 
+        {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() 
+        {
+            while (st == null || !st.hasMoreElements()) 
+            {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } 
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() 
+        {
+            String str = "";
+            try 
+            {
+                str = br.readLine();
+            } 
+            catch (IOException e) 
+            {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
