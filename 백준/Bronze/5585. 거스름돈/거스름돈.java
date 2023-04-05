4a4ed1217c374eee3637ccc55b433b718a39f781
이승석 <106569538+iamseung@users.noreply.github.com>
2023-04-05 14:01:19 +0900

    [Bronze II] Title: 거스름돈, Time: 128 ms, Memory: 14032 KB -BaekjoonHub

---
+++ 백준/Bronze/5585. 거스름돈/거스름돈.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.util.*;
+
+public class Main 
+{
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+	
+	static int N,M;
+	static int[] Money = {500, 100, 50, 10 ,5 ,1};
+
+    static void input() 
+	{
+		N = scan.nextInt();
+		M = 1000 - N;
+    }
+
+	static void pro()
+	{
+		int cnt = 0;
+
+		for(int k : Money)
+		{
+			if(M >= k)
+			{
+				cnt += M/k;
+				M = M%k;
+			}
+		}
+
+		System.out.println(cnt);
+	}
+
+    public static void main(String[] args) 
+	{
+		input();
+		pro();
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
