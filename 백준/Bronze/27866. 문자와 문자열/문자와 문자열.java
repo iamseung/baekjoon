4103cc37827d8b0099550c7598bf678c40e33686
이승석 <106569538+iamseung@users.noreply.github.com>
2023-04-09 00:13:01 +0900

    [Bronze V] Title: 문자와 문자열, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,85 @@
+import java.io.*;
+import java.util.*;
+
+public class Main 
+{
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static String str;
+    static int N;
+
+    static void input() 
+    {
+        str = scan.nextLine();
+        N = scan.nextInt();
+    }
+
+    static void pro() 
+    {
+        System.out.println(str.charAt(N-1));
+    }
+
+    public static void main(String[] args) 
+    {
+        input();
+        pro();
+    }
+
+    static class FastReader 
+    {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() 
+        {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException 
+        {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() 
+        {
+            while (st == null || !st.hasMoreElements()) 
+            {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } 
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() 
+        {
+            String str = "";
+            try 
+            {
+                str = br.readLine();
+            } 
+            catch (IOException e) 
+            {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
