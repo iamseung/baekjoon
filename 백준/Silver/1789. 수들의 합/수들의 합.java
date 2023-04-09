f5f5af12e28dd996378e4f16d812743e83019acc
이승석 <106569538+iamseung@users.noreply.github.com>
2023-04-09 23:12:41 +0900

    [Silver V] Title: 수들의 합, Time: 128 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Silver/1789. 수들의 합/수들의 합.java
@@ -0,0 +1,92 @@
+import java.io.*;
+import java.util.*;
+
+public class Main 
+{
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static long N, sum;
+
+    static void input() 
+    {
+        N = scan.nextLong();
+    }
+
+    static void pro() 
+    {
+        sum = 0;
+        long i = 0;
+
+        while(true)
+        {
+            if(sum > N) break;
+            sum += ++i;
+        }
+
+        System.out.println(i-1);
+    }
+
+    public static void main(String[] args) 
+    {
+        input();
+        pro();
+    }
+
+    static class FastReader 
+    {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() 
+        {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException 
+        {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() 
+        {
+            while (st == null || !st.hasMoreElements()) 
+            {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } 
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() 
+        {
+            String str = "";
+            try 
+            {
+                str = br.readLine();
+            } 
+            catch (IOException e) 
+            {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
