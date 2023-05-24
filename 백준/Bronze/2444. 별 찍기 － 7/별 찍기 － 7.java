f60d6e743db44d9937f91d0cf08c5ab60cd4bcab
이승석 <106569538+iamseung@users.noreply.github.com>
2023-05-24 23:54:17 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 136 ms, Memory: 14364 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,91 @@
+import java.io.*;
+import java.util.*;
+
+public class Main 
+{
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N;
+
+    static void input() 
+    {
+        N = scan.nextInt();
+    }
+
+	static void pro() 
+    {  
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N-i; j++) {
+                sb.append(" ");
+            }
+            for (int j = 1; j <= 2*i-1; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        for (int i = N-1; i >= 1; i--) {
+            for (int j = 1; j <= N-i; j++) {
+                sb.append(" ");
+            }
+            for (int j = 1; j <= 2*i-1; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);			
+	}
+
+    public static void main(String[] args) 
+    {
+       input();
+       pro();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
