86e05dd50a6b9b31f0a4fd1b524acd439972c875
이승석 <106569538+iamseung@users.noreply.github.com>
2023-06-02 17:48:53 +0900

    [Silver II] Title: 최소 힙, Time: 416 ms, Memory: 35712 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,93 @@
+import java.io.*;
+import java.util.*;
+
+public class Main 
+{
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static PriorityQueue<Integer> heap = new PriorityQueue<>();
+    static int N;
+
+    static void input() 
+    {
+        N = scan.nextInt();
+    }
+
+    static void pro()
+    {
+        while(N-- >0)
+        {
+            int M = scan.nextInt();
+            if(M == 0)
+                if(heap.size() == 0)
+                    sb.append("0\n");
+                else
+                    sb.append(heap.poll()+"\n");
+            else
+                heap.offer(M);
+        }
+
+        System.out.println(sb);
+    }
+    
+    public static void main(String[] args) 
+    {
+        input();
+        pro();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() 
+        {
+            while (st == null || !st.hasMoreElements()) 
+            {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } 
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try          
+            {
+                str = br.readLine();
+            } 
+            catch (IOException e) 
+            {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
