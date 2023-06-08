f96f331db272aaf23d77973a06f9da79e21ec33c
이승석 <106569538+iamseung@users.noreply.github.com>
2023-06-08 23:05:42 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,88 @@
+import java.io.*;
+import java.util.*;
+
+public class Main 
+{
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int N;
+
+    static void input() 
+    {
+        N = scan.nextInt();
+    }
+
+    static void pro()
+    {   
+        while(N-- > 0)
+        {
+            int S = N+1;
+            while(S-->0) sb.append("*");
+            
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) 
+    {
+        input();
+        pro();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() 
+        {
+            while (st == null || !st.hasMoreElements()) 
+            {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } 
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try          
+            {
+                str = br.readLine();
+            } 
+            catch (IOException e) 
+            {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
