9c8230cd4582c8235de529bd062660dfa4bb2873
이승석 <106569538+iamseung@users.noreply.github.com>
2023-06-08 23:24:46 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 128 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,91 @@
+import java.io.*;
+import java.util.*;
+
+public class Main 
+{
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int N;
+
+    static void input() 
+    {
+        N = scan.nextInt();
+    }
+
+    static void pro()
+    {  
+        for(int i=N; i>=1; i--)
+        {
+            int K = N;
+            while(K-- -i > 0) sb.append(" ");
+
+            int S = i;
+            while(S-->0) sb.append("*");
+
+            sb.append("\n");
+        } 
+
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) 
+    {
+        input();
+        pro();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() 
+        {
+            while (st == null || !st.hasMoreElements()) 
+            {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } 
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try          
+            {
+                str = br.readLine();
+            } 
+            catch (IOException e) 
+            {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
