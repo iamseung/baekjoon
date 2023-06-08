62814864fcc40841b630ae5b5f4cd22a463bc55d
이승석 <106569538+iamseung@users.noreply.github.com>
2023-06-08 23:28:06 +0900

    [Bronze IV] Title: 평균 점수, Time: 124 ms, Memory: 14052 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,87 @@
+import java.io.*;
+import java.util.*;
+
+public class Main 
+{
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int N = 5;
+
+    static void input() 
+    {
+        
+    }
+
+    static void pro()
+    {  
+        int ans = 0;
+        while(N-- > 0)
+        {
+            int score = scan.nextInt();
+            ans += (score<40) ? 40 : score;
+        }
+
+        System.out.println((int)ans/5);
+    }
+
+    public static void main(String[] args) 
+    {
+        input();
+        pro();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() 
+        {
+            while (st == null || !st.hasMoreElements()) 
+            {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } 
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try          
+            {
+                str = br.readLine();
+            } 
+            catch (IOException e) 
+            {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
