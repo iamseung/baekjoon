ca0cd1710f219cd3c89c6f656c763af4138dbbe7
이승석 <106569538+iamseung@users.noreply.github.com>
2023-06-28 10:44:08 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 148 ms, Memory: 15900 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.util.*;
+
+public class Main 
+{
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int[] alpha;
+    static String word;
+
+    static void input() 
+    {
+        word = scan.nextLine();
+    }
+
+    static void pro()
+    {
+        alpha = new int[26];
+
+        for(char ch : word.toCharArray())
+            alpha[ch - 'a']++;
+
+        for(int k : alpha)
+            sb.append(k+" ");
+
+        System.out.println(sb);
+    }
+    
+    public static void main(String[] args) 
+    {
+        input();
+        pro();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() 
+        {
+            while (st == null || !st.hasMoreElements()) 
+            {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } 
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try          
+            {
+                str = br.readLine();
+            } 
+            catch (IOException e) 
+            {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
