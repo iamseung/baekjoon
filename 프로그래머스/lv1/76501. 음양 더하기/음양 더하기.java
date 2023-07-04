3f3868e38356d101e4ba2a031944905a09911e07
이승석 <106569538+iamseung@users.noreply.github.com>
2023-07-04 21:03:04 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -1,12 +1,9 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
-        for(int i=0; i<absolutes.length; i++) {
-            if(signs[i])
-                answer += absolutes[i];
-            else
-                answer -= absolutes[i];
-        }
+        for(int i=0; i<absolutes.length; i++)
+            answer += absolutes[i]*(signs[i] ? 1 : -1);
+        
         return answer;
     }
 }
