61df3c57bbc8b87eb7b9f0f1a1e3d829b992d00a
이승석 <106569538+iamseung@users.noreply.github.com>
2023-07-21 09:11:28 +0900

    [unrated] Title: 대충 만든 자판, Time: 0.44 ms, Memory: 65.5 MB -BaekjoonHub

--- 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.java
@@ -1,29 +1,27 @@
-import java.util.*;
-
 class Solution {
+    static int[] alpha = new int[26];
     public int[] solution(String[] keymap, String[] targets) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        int[] answer = new int[targets.length];
-        
+        // 초기화
+        for(int i=0; i<26; i++) alpha[i] = Integer.MAX_VALUE;
+        // 최소 index 저장
         for(String str : keymap) {
             for(int i=0; i<str.length(); i++) {
-                char c = str.charAt(i);
-                if(!map.containsKey(c) || i < map.get(c)) {
-                    map.put(c, i+1);
-                }
+                // 저장되어 있는 값과 현재 인덱스 중 최솟값 저장
+                alpha[str.charAt(i)-'A'] = Math.min(alpha[str.charAt(i)-'A'], i);
             }
         }
-        
+        // 정답배열
+        int[] answer = new int[targets.length];
         // 비교
         for(int i=0; i<targets.length; i++) {
             for(int j=0; j<targets[i].length(); j++) {
-                if(!map.containsKey(targets[i].charAt(j))) {
+                if(alpha[targets[i].charAt(j)-'A'] == Integer.MAX_VALUE) {
                     answer[i] = -1;
                     break;
                 }
                 
                 // 0부터 인덱스를 잡기 때문에 +1
-                answer[i] += map.get(targets[i].charAt(j));
+                answer[i] += alpha[targets[i].charAt(j)-'A']+1;
             }
         }
         
