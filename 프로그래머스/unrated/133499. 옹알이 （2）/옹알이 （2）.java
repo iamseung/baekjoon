06001cc92463a1e7c6496fd829a977e2f4d57feb
이승석 <106569538+iamseung@users.noreply.github.com>
2023-07-25 10:59:17 +0900

    [unrated] Title: 옹알이 (2), Time: 2.86 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,13 +1,13 @@
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-        for(String s : babbling) {
-            // 연달아 발음하는 경우
-            s = s.replaceAll("ayaaya|yeye|woowoo|mama", " ");
+        for(String str : babbling) {
+            str = str.replace("aya", "");
+            str = str.replace("ye", "");
+            str = str.replace("woo", "");
+            str = str.replace("ma", "");
             
-            s = s.replaceAll("aya|ye|woo|ma", "");
-            
-            if(s.length() == 0) answer++;
+            if(str.length() == 0) answer++;
         }
         return answer;
     }
