b533721f6617c58f9f19230e53b79b1538164424
이승석 <106569538+iamseung@users.noreply.github.com>
2023-08-17 10:18:18 +0900

    [Gold IV] Title: 알파벳, Time: 896 ms, Memory: 15444 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,73 +1,50 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-import java.util.Queue;
-import java.util.LinkedList;
-
-/*
-1. 같은 알파벳을 두번 지날 수 없으니 중복을 허용하지 않는 재귀함수이다.
-  - 1차원 방문배열 사용(알파벳 26개)
-2. 가지치기를 통한 재귀함수이니 백트래킹 알고리즘이다.
-3. 가지치기
-  - 맵 범위를 벗어나는 경우
-  - 이미 방문한 알파벳인 경우
-*/
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int R, C;
-    static boolean visited[];
-    static char map[][];
-    static int max_count = Integer.MIN_VALUE;
-    static int dx[] = {-1, 0, 1, 0};
-    static int dy[] = {0, 1, 0, -1};
-
-    void DFS(int x, int y, int count) {
-        int idx = map[x][y] - 'A';
-        visited[idx] = true;
-        max_count = Math.max(max_count, count);
-        // 탐색시작
-        for (int i=0; i<4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            // 가지치기
-            if (nx<0 || nx>=R || ny<0 || ny>=C) continue; // 범위 벗어나면 패스
-            idx = map[nx][ny] - 'A';
-            if (visited[idx]) continue; // 방문한 알파벳이면 패스
-            // 탐색진행
-            DFS(nx, ny, count+1);
-            // 백트래킹으로 이전 노드로 복귀하면 다음을 위해 방문처리 취소
-            visited[idx] = false;
-            
-        }
-        
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int N,M,ans=0;
+    static char[][] map;
+    static int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
+    static boolean[] alpha = new boolean[26];
+    public static void main(String[] args) throws IOException {
+        input();
+        pro();
     }
 
-	void Solve() {
-        visited = new boolean[26];
-        DFS(0, 0, 1);
-        System.out.println(max_count);
-	}
+    public static void input() throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new char[N][M];
 
-    void inputData() throws Exception {
-        InputStreamReader reader = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(reader);
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        map = new char[R][C];
-        for (int i=0; i<R; i++) {
-            //st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
             String str = br.readLine();
-            for (int j=0; j<C; j++) {
-                //map[i][j] = Integer.parseInt(st.nextToken());
+            for(int j=0; j<M; j++) {
                 map[i][j] = str.charAt(j);
             }
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        Main m = new Main();
-        m.inputData(); // 입력 받는 부분
-        m.Solve();// 여기서부터 작성
+    public static void pro() throws IOException {
+        dfs(0,0,1);
+        System.out.println(ans);
+    }
+
+    public static void dfs(int x, int y, int count) {
+        alpha[map[y][x]-'A'] = true;
+        ans = Math.max(ans, count);
+        for(int k=0; k<4; k++) {
+            int nx = x + dir[k][0];
+            int ny = y + dir[k][1];
+
+            if(nx<0 || ny<0|| nx>=M || ny>=N) continue;
+            if(alpha[map[ny][nx]-'A']) continue;
+
+            dfs(nx, ny, count+1);
+
+            alpha[map[ny][nx]-'A'] = false;
+        }
     }
 }
