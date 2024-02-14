f5890400225b755f8854b7176f09d143675e1c8f
이승석 <106569538+iamseung@users.noreply.github.com>
2024-02-14 13:42:35 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 8.27 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+class Solution {
+     public int solution(String str1, String str2) {
+
+        //소문자 대문자 상관없기 때문에 소문자로 만들기
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+
+        //str1의 단어 리스트
+        List<String> list1 = new ArrayList<>();
+
+        //str2의 단어 리스트
+        List<String> list2 = new ArrayList<>();
+
+        //교집합 리스트
+        List<String> intersection = new ArrayList<>();
+
+        //합집합 리스트
+        List<String> union = new ArrayList<>();
+
+        //str1 두 단어씩 나눠서 list1 에 넣기
+        for (int i = 0; i < str1.length() - 1; i++) {
+            char c1 = str1.charAt(i);
+            char c2 = str1.charAt(i + 1);
+            if (c1 >= 'a' && c1 <= 'z') {
+                if (c2 >= 'a' && c2 <= 'z') {
+                    list1.add(c1 + "" + c2);
+                }
+            }
+        }
+
+        //str2 두 단어씩 나눠서 list2 에 넣기
+        for (int i = 0; i < str2.length() - 1; i++) {
+            char c1 = str2.charAt(i);
+            char c2 = str2.charAt(i + 1);
+            if (c1 >= 'a' && c1 <= 'z') {
+                if (c2 >= 'a' && c2 <= 'z') {
+                    list2.add(c1 + "" + c2);
+                }
+            }
+        }
+
+        Collections.sort(list1);
+        Collections.sort(list2);
+
+        for(String s : list1){
+            if(list2.remove(s)){
+                intersection.add(s);
+            }
+            union.add(s);
+        }
+
+        for(String s : list2){
+            union.add(s);
+        }
+
+        double answer = 0;
+
+        if(union.size() == 0) {
+            answer = 1;
+        } else {
+            answer = (double) intersection.size() / (double) union.size();
+        }
+
+        return (int) (answer * 65536);
+    }
+}
