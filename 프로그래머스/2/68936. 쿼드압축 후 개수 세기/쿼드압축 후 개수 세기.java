208a5a3b2f036df37890ac494a94c59610951294
이승석 <106569538+iamseung@users.noreply.github.com>
2024-03-11 16:34:51 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 4.60 ms, Memory: 103 MB -BaekjoonHub

---
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -0,0 +1,35 @@
+class Solution {
+    static int[] answer;
+    
+    public int[] solution(int[][] arr) {
+        answer = new int[2];
+        quad(arr,0,0,arr.length);
+        return answer;
+    }
+    
+     public boolean zip(int[][] arr, int x, int y, int size, int val) {
+        for(int i = x; i < x + size; i++) {
+            for(int j = y; j < y + size; j++) {
+                if(arr[i][j] != val)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void quad(int[][] arr, int x, int y, int size){
+        if(zip(arr,x,y,size, arr[x][y])){
+            if(arr[x][y] == 1)
+                answer[1]++;
+            else answer[0]++;
+            return ;
+        }
+
+        // 4분할 quad 재귀
+        quad(arr,x,y, size/2);
+        quad(arr,x,y + size/2, size/2);
+        quad(arr,x+size/2,y, size/2);
+        quad(arr,x+size/2,y + size/2, size/2);
+    }
+}
