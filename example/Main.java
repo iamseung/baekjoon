6f0a5e48b177ede3e3182c7ed1191aa3261d5950
iamseung <seesungseok@naver.com>
2024-04-01 09:35:44 +0900

    코드 샘플 작성

---
+++ example/Main.java
@@ -0,0 +1,21 @@
+package example;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        input();
+        simulate();
+    }
+
+    static void input() throws IOException  {
+
+    }
+
+    
+    static void simulate() {
+       
+    }
+}
---
+++ example/Solution.java
@@ -0,0 +1,22 @@
+package example;
+
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        input();
+        simulate();
+    }
+
+    static void input() throws IOException  {
+
+    }
+
+    
+    static void simulate() {
+       
+    }
+}
