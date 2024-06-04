b3c81077e92a18cd2e37b1cc5effcc05e660b3f2
이승석 <106569538+iamseung@users.noreply.github.com>
2024-06-04 23:13:15 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.04 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -2,6 +2,7 @@ class Solution {
     public int solution(String my_string) {
         int answer = 0;
         for(char s : my_string.toCharArray()) {
+            // 숫자 판별
             if(Character.isDigit(s))
                 answer += s-'0';
         }
