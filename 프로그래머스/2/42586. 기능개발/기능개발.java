d9e41644152abcde752f6dec35ec5595a028efca
이승석 <106569538+iamseung@users.noreply.github.com>
2024-06-23 23:57:28 +0900

    [level 2] Title: 기능개발, Time: 2.71 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,33 +2,31 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> answer = new ArrayList<>();
-        Queue<Integer> Q = new LinkedList<>();
-        
+        List<Integer> ans = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
         for(int i=0; i<progresses.length; i++) {
-            int N = (100 - progresses[i]) / speeds[i];
-            if((100 - progresses[i]) % speeds[i] != 0) N++;
-            Q.add(N);
-        }
-        
-        while(!Q.isEmpty()) {
-            int T = Q.poll(); // 7
-            int cnt = 1;
-            while(!Q.isEmpty()) {
-                if(T >= Q.peek()) {
-                    Q.poll();
-                    cnt++;
-                } else {
-                    break;
-                }
-            }
+            int gap = (100-progresses[i])/speeds[i];
+            if((100-progresses[i])%speeds[i] != 0)
+                gap++;
+
+            queue.offer(gap);
             
-            answer.add(cnt);
         }
-        
-        int[] arr = answer.stream()
-	        .mapToInt(Integer::intValue)
-    	    .toArray();
-        return arr;
+
+        while(!queue.isEmpty()) {
+            int cur = queue.poll();
+
+            int cnt = 1;
+            while(!queue.isEmpty() && queue.peek() <= cur) {
+                queue.poll();
+                cnt++;
+            }
+
+            ans.add(cnt);
+        }
+
+        return ans.stream()
+                    .mapToInt(Integer::intValue)
+                    .toArray();
     }
 }
