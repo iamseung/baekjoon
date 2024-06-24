ba80a1ee6a69c2c7f73e622f9cfceda85b3e236a
이승석 <106569538+iamseung@users.noreply.github.com>
2024-06-25 01:20:03 +0900

    [level 2] Title: 프로세스, Time: 0.72 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,24 +2,27 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        int answer = 0;
-        // 내림차순 정렬
+        int answer = 0, len = priorities.length;
+
+        // 우선 순위 내림차순
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-		
-		for(int num : priorities) {
-			pq.add(num);
-		}
-        
-		while(!pq.isEmpty()) {
-			for(int i=0; i<priorities.length; i++) {
-				if(priorities[i] == pq.peek()) {
-					pq.poll();
-					answer++;
-					if(i == location)
-						return answer;
-				}
-			}
-		}  
-        return answer;
+
+        for(int p : priorities)
+            pq.add(p);
+
+        while(!pq.isEmpty()) {
+            for(int i=0; i<len; i++) {
+                // 우선 순위가 높은 순으로 조회, 같은 우선 순위인 경우
+                if(priorities[i] == pq.peek()) {
+                    pq.poll();
+                    answer++;
+                    // 찾는 index 의 값
+                    if(i == location)
+                        return answer;
+                }
+            }
+        }
+
+        return answer; 
     }
 }
