faedba5c7d3fefef3c430a9ae39901dba0dfbb46
이승석 <106569538+iamseung@users.noreply.github.com>
2024-06-27 22:28:35 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.63 ms, Memory: 53.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,58 +1,40 @@
 import java.util.*;
 
 class Solution {
-    static boolean[][] visit;
-    static int N, M;
-    static int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
-    static int[][] dist;
+    static int[][] dir = {{1,0},{0,1},{-1,0},{0,-1}};
+    static int dirLen = 4;
+    
     public int solution(int[][] maps) {
-        N = maps.length;
-        M = maps[0].length;
+        int Y = maps.length;
+        int X = maps[0].length;
+        int[][] dist = new int[Y][X];
 
-        dist = new int[N][M];
-        
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<M; j++) {
-                dist[i][j] = -1;
-            }
-        }
-    
-        return BFS(maps);
-    }
-    
-    static int BFS(int[][] maps) {
-        Queue<Integer> que = new LinkedList<>();
-        
-        int ANS = -1;
-        
-        que.add(0);
-        que.add(0);
+        for(int y=0; y<Y; y++)
+            Arrays.fill(dist[y], -1);
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0,0});
+
         dist[0][0] = 1;
-        
-        while(!que.isEmpty()) {
-            int x = que.poll();
-            int y = que.poll();
-            
-            if(x == M-1 &&  y == N-1) {
-                ANS = dist[y][x];
-                break;
-            }
-            
-            for(int k=0; k<4; k++) {
-                int nx = x + dir[k][0];
-                int ny = y + dir[k][1];
 
-                if(nx<0 || ny<0 || nx>=M || ny>=N) continue;
+        while(!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int x = cur[1];
+            int y = cur[0];
+
+            for(int k=0; k<dirLen; k++) {
+                int nx = x + dir[k][1];
+                int ny = y + dir[k][0];
+
+                if(nx < 0 || ny < 0 || nx >= X || ny >= Y) continue;
                 if(dist[ny][nx] != -1) continue;
                 if(maps[ny][nx] == 0) continue;
-                
-                que.add(nx);
-                que.add(ny);
-                
+
                 dist[ny][nx] = dist[y][x] + 1;
+                queue.offer(new int[]{ny, nx});
             }
         }
-        
-        return ANS;
+
+        return dist[Y-1][X-1];
     }
 }
