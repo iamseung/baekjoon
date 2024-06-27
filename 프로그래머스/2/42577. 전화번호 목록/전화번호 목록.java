8be65b002eb2775d70513236e66c63a1b3441f6e
이승석 <106569538+iamseung@users.noreply.github.com>
2024-06-27 14:19:27 +0900

    [level 2] Title: 전화번호 목록, Time: 280.14 ms, Memory: 96.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,12 +2,11 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        // 숫자 오름차순 -> 길이 오름차순
         Arrays.sort(phone_book);
-        
-        // 앞 번호가 뒷 번호의 접두어인지 체크
-        for(int i=0; i<phone_book.length -1; i++) {
-            if(phone_book[i+1].startsWith(phone_book[i])) return false;
+
+        for(int i=0; i<phone_book.length-1; i++) {
+            if(phone_book[i+1].startsWith(phone_book[i]))
+                return false;
         }
         
         return true;
