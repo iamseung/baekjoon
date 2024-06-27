b2074ffbc7b620e28a08690f43f2f19ea3fa662f
이승석 <106569538+iamseung@users.noreply.github.com>
2024-06-27 14:46:21 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.48 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,47 +1,42 @@
 import java.util.*;
 
 class Solution {
+    static final int popMax = 1;
+    static final int popMin = -1;
+    static final String INSERT = "I";
+    static final String DELETE = "D";
+    
     public int[] solution(String[] operations) {
-        int[] answer = new int[2];
-        PriorityQueue<Integer> minQ = new PriorityQueue<>();
-        PriorityQueue<Integer> maxQ = new PriorityQueue<>(Collections.reverseOrder());
-        
-        StringTokenizer st;
-        String type;
-        int N;
-        for(String o : operations) {
-            
-            st = new StringTokenizer(o, " ");
-            type = st.nextToken();
-            N = Integer.parseInt(st.nextToken());
+        PriorityQueue<Integer> maxQ = new PriorityQueue<>(Collections.reverseOrder()); // 큰 것부터 pop
+        PriorityQueue<Integer> minQ = new PriorityQueue<>(); // 작은 것부터 pop
 
-            if(type.equals("I")) {
-                minQ.add(N);
-                maxQ.add(N);
+        StringTokenizer st;
+        for(String s : operations) {
+            st = new StringTokenizer(s, " ");
+            String command = st.nextToken();
+            int value = Integer.parseInt(st.nextToken());
+
+            if(command.equals(INSERT)) {
+                maxQ.offer(value);
+                minQ.offer(value);
             }
-            else {
-                if(minQ.size() != 0) {
-                    // 최대값 삭제
-                    if(N == 1) {
-                        int max = maxQ.poll();
-                        minQ.remove(max);
-                    }
-                    // 최솟값 삭제
-                    else {
-                        int min = minQ.poll();
-                        maxQ.remove(min);
-                    }  
+
+            if(command.equals(DELETE)) {
+                // 최댓값 삭제
+                if(value == popMax) {
+                    minQ.remove(maxQ.poll());
+                }
+
+                // 최솟값 삭제
+                if(value == popMin) {
+                    maxQ.remove(minQ.poll());
                 }
             }
         }
-        
-        if(minQ.size() == 0 || maxQ.size() == 0) {
-           answer = new int[]{0,0};
-        } else {
-            answer[1] = minQ.poll();
-            answer[0] = maxQ.poll();   
-        }
-         
-        return answer;
+
+        if(maxQ.isEmpty() || minQ.isEmpty())
+            return new int[]{0,0};
+
+        return new int[]{maxQ.poll(), minQ.poll()};
     }
 }
