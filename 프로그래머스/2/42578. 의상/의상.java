69621c9d3d09bffe717e7ca47dd7bbf667aaf194
이승석 <106569538+iamseung@users.noreply.github.com>
2024-06-27 11:46:06 +0900

    [level 2] Title: 의상, Time: 0.08 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,16 +2,15 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        Map<String, Integer> map = new HashMap<>();
         int answer = 1;
+        Map<String, Integer> cloMap = new HashMap<>();
         
-        for(String[] c : clothes) {
-            map.put(c[1], map.getOrDefault(c[1], 0) + 1);
-        }
-        
-        for(String key : map.keySet()) {
-            answer *= map.get(key) + 1; // 아무것도 입지 않는 경우 = 1
-        }
+        // 의류 별 갯수 체크
+        for(String[] c : clothes)
+            cloMap.put(c[1], cloMap.getOrDefault(c[1], 0) + 1);
+
+        for(String key : cloMap.keySet())
+            answer *= cloMap.get(key)+1;
         
         return answer - 1;
     }
