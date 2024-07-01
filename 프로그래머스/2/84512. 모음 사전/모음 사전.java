d643dd1892407fa31772499f85c6bfb20d2e34b7
이승석 <106569538+iamseung@users.noreply.github.com>
2024-07-01 15:29:40 +0900

    [level 2] Title: 모음 사전, Time: 4.73 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,27 +1,26 @@
-import java.util.*;
-
 class Solution {
     static int answer = 0;
     static int depth = 0;
+    static final int len = 5;
     static String[] alpha = {"A","E","I","O","U"};
     
     public int solution(String word) {
-        answer = 0;
         dfs("", word);
         return answer;
     }
+    
+    static void dfs(String st, String word) {
+        if(st.length() > len)
+            return;
 
-    private void dfs(String st, String word) {
-        if (st.length() > 5) return;
-        if (st.equals(word)) {
+        if(st.equals(word)) {
             answer = depth;
             return;
         }
-        
+
         depth++;
-        
-        for(int i=0; i<5; i++) 
+
+        for(int i=0; i<len; i++)
             dfs(st + alpha[i], word);
-        
     }
 }
