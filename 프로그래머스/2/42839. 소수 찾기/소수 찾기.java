38568696bd7c88eb89c1860ba80bbd7b095abb8b
이승석 <106569538+iamseung@users.noreply.github.com>
2024-07-02 14:30:28 +0900

    [level 2] Title: 소수 찾기, Time: 204.93 ms, Memory: 94.8 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,31 +1,31 @@
 import java.util.*;
 
 class Solution {
+    static final int len = 10000000;
+    static boolean[] prime = new boolean[len];
     static Set<Integer> resultList = new HashSet<>();
     
-    public int solution(String numbers) { 
+    public int solution(String numbers) {
+        makePrimeArr();
+
         int len = numbers.length();
-        int[] arr = new int[len];
+        char[] arr = numbers.toCharArray();
 
-        for(int i=0; i<len; i++) 
-            arr[i] = numbers.charAt(i) - '0';
-            
-        // 1개부터 모두 고르는 조합을 구한다.
-        for(int i=1; i<=len; i++) 
+        for(int i=1; i<=len; i++)
             permutation(arr, 0, len, i);
-
+        
         return resultList.size();
     }
     
-    // n개 중에서 r개를 순서있게 뽑기
-    static void permutation(int[] arr, int depth, int n, int r) {
+    // n 개 중에 r 개
+    static void permutation(char[] arr, int depth, int n, int r) {
         if(depth == r) {
             String number = "";
             for(int i=0; i<r; i++) {
                 number += arr[i];
             }
 
-            if(isPrime(Integer.valueOf(number))) 
+            if(!prime[Integer.valueOf(number)]) 
                 resultList.add(Integer.valueOf(number));
 
             return;
@@ -37,23 +37,19 @@ class Solution {
             swap(arr, depth, i);
         }
     }
-    
-    static void swap(int[] arr, int depth, int i) {
-        int temp = arr[depth];
+
+    static void swap(char[] arr, int depth, int i) {
+        char temp = arr[depth];
         arr[depth] = arr[i];
         arr[i] = temp;
     }
 
-    static boolean isPrime(int N) {
-        if(N < 2) return false;
+    static void makePrimeArr() {
+        prime[0] = prime[1] = true;
 
-        for(int i=2; i<=(int) Math.sqrt(N); i++) {
-            if(N % i == 0) {
-                return false;
-            }
+        for(int i=2; i<=(int) Math.sqrt(len); i++) {
+            for(int j=2; i*j<len; j++)
+                prime[i*j] = true;
         }
-
-        return true;
     }
-    
 }
