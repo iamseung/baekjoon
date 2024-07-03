395b4b3f04c1e17b6fb3701bc81ddb4066800a02
이승석 <106569538+iamseung@users.noreply.github.com>
2024-07-03 11:36:10 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 6.76 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -4,57 +4,45 @@ class Solution {
     static boolean[][] graph;
     
     public int solution(int n, int[][] wires) {
-        conn(n, wires);
-        
         int answer = n;
-        
-        int a,b;
+        graph = new boolean[n+1][n+1];
+
+        for(int[] w : wires) 
+            graph[w[0]][w[1]] = graph[w[1]][w[0]] = true;
+
         for(int[] w : wires) {
-            a = w[0];
-            b = w[1];
-            
-            graph[a][b] = false;
-            graph[b][a] = false;
-            
+            int a = w[0], b = w[1];
+            graph[a][b] = graph[b][a] = false;
             answer = Math.min(answer, bfs(n,a));
-            
-            graph[a][b] = true;
-            graph[b][a] = true;
+            graph[a][b] = graph[b][a] = true;
         }
         
         return answer;
     }
-    
-    public int bfs(int n, int start) {
+
+    static int bfs(int n, int start) {
         boolean[] visit = new boolean[n+1];
         int cnt = 1;
-        
-        Queue<Integer> Q = new LinkedList<>();
-        Q.offer(start);
-        
-        while(!Q.isEmpty()) {
-            int point = Q.poll();
-            visit[point] = true;
-            
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start);
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            visit[cur] = true;
+
             for(int i=1; i<=n; i++) {
-                if(visit[i]) continue;
-                if(graph[point][i]) {
-                    Q.offer(i);
+                if(visit[i])
+                    continue;
+                
+                if(graph[cur][i]) {
+                    queue.offer(i);
                     cnt++;
                 }
             }
         }
-        
+
+        // Math.abs(cnt - (n-cnt))
         return (int) Math.abs(n-cnt*2);
-    }
-    
-    public static void conn(int n, int[][] wires) {
-        graph = new boolean[n+1][n+1];
-        
-        // 양방향
-        for(int[] w : wires) {
-            graph[w[0]][w[1]] = true;
-            graph[w[1]][w[0]] = true;
-        }
     }
 }
