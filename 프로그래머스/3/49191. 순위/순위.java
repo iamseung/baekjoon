5c7dda88c439887d8dd777de1eea9a758a8df9dd
이승석 <106569538+iamseung@users.noreply.github.com>
2024-07-03 15:20:17 +0900

    [level 3] Title: 순위, Time: 13.20 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,30 +1,29 @@
 class Solution {
     public int solution(int n, int[][] results) {
-        int answer = 0;
-        int[][] graph = new int[n+1][n+1];
+                int answer = 0;
 
-        for(int[] r : results) 
-            graph[r[0]][r[1]] = 1;
+        boolean[][] score = new boolean[n+1][n+1];
 
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
-                for(int k=1; k<=n; k++){
-                    if(graph[j][i] == 1 && graph[i][k] == 1)
-                        graph[j][k] = 1;
+        for(int[] r : results)
+            score[r[0]][r[1]] = true;
+        
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n; j++) {
+                for(int k=1; k<=n; k++) {
+                    if(score[j][i] && score[i][k])
+                        score[j][k] = true;
                 }
             }
         }
 
         for(int i=1; i<=n; i++) {
-            int cnt = 0;
+            int count = 0;
             for(int j=1; j<=n; j++) {
-                // 둘 중 하나라도 순위를 알 수 있을 때
-                if(graph[i][j] == 1 || graph[j][i] == 1)
-                    cnt++;
+                if(score[i][j] || score[j][i])
+                    count++;
             }
 
-            // 본인을 제외한 모든 노드들과의 순위를 알 수 있을 때
-            if(cnt == n-1) 
+            if(count == n-1)
                 answer++;
         }
 
