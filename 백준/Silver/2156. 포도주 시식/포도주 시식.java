a03f4c91abc421ccc4c0fa2c7433aa85b8ff1beb
이승석 <106569538+iamseung@users.noreply.github.com>
2024-07-31 13:53:12 +0900

    [Silver I] Title: 포도주 시식, Time: 148 ms, Memory: 15040 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,27 +1,39 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N;
+    static int[] wine;
+    static int[] dp;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		
-		int[] wine = new int[n+1];
-		for(int i = 1; i < n+1; i++) {
-			wine[i] = sc.nextInt();
-		}
-		
-		int[] dp = new int[n+1];
-		dp[1] = wine[1];
-		if(n > 1) { // N=1의 경우를 대비해 예외처리 
-			dp[2] = wine[1] + wine[2];
-		}
-	
-		for(int i = 3; i < n+1; i++) { // 전 와인과 전전 와인을 먹을 수 있는 3번째 와인부터 시작 
+	public static void main(String[] args) throws Exception {
+		input();
+        process();
+	}
+
+    static void input() throws Exception {
+        N = Integer.parseInt(br.readLine());
+
+        wine = new int[N+1];
+        for(int i=1; i<=N; i++)
+            wine[i] = Integer.parseInt(br.readLine());
+
+        dp = new int[N+1];
+        dp[1] = wine[1];
+    }
+
+    static void process() {
+        // N=1의 경우를 대비해 예외처리 
+        if(N > 1)
+            dp[2] = wine[1] + wine[2];
+
+        // 전 와인과 전전 와인을 먹을 수 있는 3번째 와인부터 시작 
+        for(int i = 3; i <= N; i++) { 
 			dp[i] = Math.max(dp[i-1], Math.max(dp[i-2], dp[i-3] + wine[i-1]) + wine[i]); 
 		}
 		
-		System.out.println(dp[n]);
-	}
+		System.out.println(dp[N]);
+    }
 
 }
