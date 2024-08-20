f72c93df588d632c096d7a5a1bcb4975034e6594
이승석 <106569538+iamseung@users.noreply.github.com>
2024-08-20 15:58:18 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 11.31 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,16 +1,17 @@
 class Solution {
-    static int[] answer;
-    
+    static int[] answer = new int[2];
+    static int[][] map;
+
     public int[] solution(int[][] arr) {
-        answer = new int[2];
-        quad(arr,0,0,arr.length);
+        map = arr;
+        quad(0, 0, arr.length);
         return answer;
     }
-    
-     public boolean zip(int[][] arr, int x, int y, int size, int val) {
-        for(int i = x; i < x + size; i++) {
-            for(int j = y; j < y + size; j++) {
-                if(arr[i][j] != val)
+
+    public boolean zip(int x, int y, int size, int val) {
+        for (int i = x; i < x + size; i++) {
+            for (int j = y; j < y + size; j++) {
+                if (map[i][j] != val)
                     return false;
             }
         }
@@ -18,18 +19,23 @@ class Solution {
         return true;
     }
 
-    public void quad(int[][] arr, int x, int y, int size){
-        if(zip(arr,x,y,size, arr[x][y])){
-            if(arr[x][y] == 1)
+    public void quad(int x, int y, int size) {
+        // (x,y) 좌표부터 size 만큼의 구역을 압축할 수 있으면
+        if (zip(x, y, size, map[x][y])) {
+            if (map[x][y] == 1)
                 answer[1]++;
-            else answer[0]++;
-            return ;
+            else
+                answer[0]++;
+            
+            return;
         }
 
         // 4분할 quad 재귀
-        quad(arr,x,y, size/2);
-        quad(arr,x,y + size/2, size/2);
-        quad(arr,x+size/2,y, size/2);
-        quad(arr,x+size/2,y + size/2, size/2);
+        int half = size / 2;
+
+        quad(x, y, half);
+        quad(x, y + half, half);
+        quad(x + half, y, half);
+        quad(x + half, y + half, half);
     }
 }
