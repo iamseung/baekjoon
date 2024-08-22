acd67e5d1ed984136a0e5ce3a91d6a1dbe645ba2
이승석 <106569538+iamseung@users.noreply.github.com>
2024-08-22 19:36:34 +0900

    [level 3] Title: 숫자 게임, Time: 65.30 ms, Memory: 67 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -8,12 +8,19 @@ class Solution {
 
         Arrays.sort(A);
         Arrays.sort(B);
-        for(int i=len; i>=0; i--) {
-            if(B[index] > A[i]) {
+
+        for (int i = len; i >= 0; i--) {
+            // B 팀원이 이기는 경우
+            if (B[index] > A[i]) {
+                // 승점 +1
                 answer++;
+                // B 의 Max 값 소모
                 index--;
             }
+
+            // 비기거나 B 의 수가 더 낮을 경우, 승점 X
         }
+
         return answer;
     }
 }
