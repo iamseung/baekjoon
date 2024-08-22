f6de485081f49aeb42fa285c43d9ae812dfdb857
이승석 <106569538+iamseung@users.noreply.github.com>
2024-08-22 21:08:03 +0900

    [level 3] Title: 최고의 집합, Time: 1.76 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -2,17 +2,21 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, int s) {
-        if(n>s)
-            return new int[]{-1};
-        
+        if (n > s)
+            return new int[] { -1 };
+
         int[] answer = new int[n];
-            Arrays.fill(answer, s/n);
-        
-        for(int i=0; i<s%n; i++)
+
+        // s 를 n 개에 적절히 분배
+        Arrays.fill(answer, s / n);
+
+        // 남은 만큼 추가로 분배
+        for (int i = 0; i < s % n; i++)
             answer[i]++;
-        
+
+        // 오름차순 정렬
         Arrays.sort(answer);
-        
+
         return answer;
     }
 }
