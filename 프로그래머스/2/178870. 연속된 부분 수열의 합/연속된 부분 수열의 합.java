a4d352158e078fd8c34400ca54d8271fb23e3acc
이승석 <106569538+iamseung@users.noreply.github.com>
2024-08-23 19:27:56 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 29.62 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] sequence, int k) {
-         int L = 0, R = 0, sum = 0;
-        int minLength = Integer.MAX_VALUE;
-        int[] answer = {-1, -1};
+        int L = 0, R = 0, sum = 0;
+        int gap = Integer.MAX_VALUE;
+        int[] answer = { -1, -1 };
 
         while (R < sequence.length) {
             // sum을 k에 맞추거나 k보다 크게 만들기 위해 R을 이동시키기
@@ -12,14 +12,15 @@ class Solution {
                 sum += sequence[R++];
             }
 
-            // 합이 k 이상인 경우, 최소 길이를 찾으며 L을 이동시키기
+            // 합이 k 이상인 경우
             while (sum >= k) {
-                if (sum == k && (R - L) < minLength) {
-                    minLength = R - L;
+                // 최소 길이를 찾으며 L을 이동시키기
+                if (sum == k && (R - L) < gap) {
+                    gap = R - L;
                     answer[0] = L;
                     answer[1] = R - 1; // R은 이미 다음 위치를 가리키므로 -1 조정
                 }
-                
+
                 sum -= sequence[L++];
             }
         }
