db9efdc67bb8f7577d5a5e0fbff8277ee4e37211
이승석 <106569538+iamseung@users.noreply.github.com>
2024-08-23 15:00:39 +0900

    [level 3] Title: 단속카메라, Time: 9.96 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -4,20 +4,20 @@ class Solution {
     public int solution(int[][] routes) {
         int answer = 1;
 
-        // 시작 지점을 기준으로 오름차순
-        Arrays.sort(routes, (a,b) -> (a[1] - b[1]));
+        // 종료 지점을 기준으로 오름 차순
+        Arrays.sort(routes, (a, b) -> a[1] - b[1]);
 
-        // 첫번째의 끝나는 지점을 기준으로 시작
         int last = routes[0][1];
 
-        for(int i=1; i<routes.length; i++) {
-            if(routes[i][0] <= last && routes[i][1] >= last)
+        for (int i = 1; i < routes.length; i++) {
+            // 하나의 카메라로 다음 구간을 측정할 수 있다면 pass
+            if (routes[i][0] <= last && last <= routes[i][1])
                 continue;
-            
+
             last = routes[i][1];
             answer++;
         }
-        
+
         return answer;
     }
 }
