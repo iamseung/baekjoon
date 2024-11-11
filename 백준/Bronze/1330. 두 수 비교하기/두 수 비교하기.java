c1bcfe0449543e1514539133ced8771d5557afeb
이승석 <106569538+iamseung@users.noreply.github.com>
2024-11-11 22:58:21 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -1,30 +1,30 @@
-import java.util.Scanner;
-import java.util.StringTokenizer;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
 
-public class Main {
-	public static void main(String[] args) throws IOException {
-		// System.in 은 입력한 값을 Byte 단위로 읽는 것을 뜻한다.
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+class Main {
 
-		String str = br.readLine();
-		StringTokenizer st = new StringTokenizer(str, " ");
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int A, B;
 
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws Exception {
+        input();
+        process();
+    }
 
-		if (a < b) {
-			System.out.println("<");
-		}
+    static void input() throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+    }
 
-		if (a > b) {
-			System.out.println(">");
-		}
-
-		if (a == b) {
-			System.out.println("==");
-		}
-	}
+    static void process() {
+        if (A > B) {
+            System.out.println(">");
+        } else if (A < B) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+    }
 }
