2718a75c047185b3bd55e670741766a89dd8e11e
이승석 <iamseung@kakao.com>
2025-05-28 11:58:27 +0900

    [Gold V] Title: 보물섬, Time: 616 ms, Memory: 298016 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.java
+++ 백준/Gold/2589. 보물섬/보물섬.java
@@ -3,9 +3,10 @@ import java.io.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static int N,M,ANS;
-    static int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
+    static int N, M, ANS;
+    static int[][] dir = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
     static char[][] area;
+
     public static void main(String[] args) throws IOException {
         input();
         process();
@@ -19,21 +20,20 @@ public class Main {
         area = new char[N][M];
 
         String str;
-        for(int i=0; i<N; i++) {
+        for (int i = 0; i < N; i++) {
             str = br.readLine();
-            for(int j=0; j<M; j++) {
+            for (int j = 0; j < M; j++) {
                 area[i][j] = str.charAt(j);
-                // System.out.println(area[i][j]);
             }
         }
     }
 
     public static void process() {
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<M; j++) {
-                // 육지인 곳에서 제일 
-                if(area[i][j] == 'L') {
-                    BFS(j,i);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                // 육지인 곳에서 제일
+                if (area[i][j] == 'L') {
+                    BFS(j, i);
                 }
             }
         }
@@ -43,8 +43,8 @@ public class Main {
 
     public static void BFS(int x, int y) {
         int[][] dist = new int[N][M];
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<M; j++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
                 dist[i][j] = -1;
             }
         }
@@ -55,27 +55,29 @@ public class Main {
 
         dist[y][x] = 0;
 
-        while(!Q.isEmpty()) {
+        while (!Q.isEmpty()) {
             int qx = Q.poll();
             int qy = Q.poll();
 
-            for(int k=0; k<4; k++) {
+            for (int k = 0; k < 4; k++) {
                 int nx = qx + dir[k][0];
                 int ny = qy + dir[k][1];
-                
-                if(nx<0 || ny<0 || nx>=M || ny>=N) continue;
-                if(area[ny][nx] != 'L') continue;
-                if(dist[ny][nx] != -1) continue;
+
+                if (nx < 0 || ny < 0 || nx >= M || ny >= N)
+                    continue;
+                if (area[ny][nx] != 'L')
+                    continue;
+                if (dist[ny][nx] != -1)
+                    continue;
 
                 Q.add(nx);
                 Q.add(ny);
 
                 dist[ny][nx] = dist[qy][qx] + 1;
-
+                // 최단 ㅓ리로 이동하는데 가장 긴 시간이 걸리는 곳
                 ANS = Math.max(ANS, dist[ny][nx]);
             }
         }
-
 
     }
 }
